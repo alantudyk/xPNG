cb3290cc3f6e8e68c76dc59ff429b17f5e7da8d0
Alan Tudyk <78517110+alantudyk@users.noreply.github.com>
2021-03-22 17:54:34 +0300

    sintel_trailer_test

---
+++ ancestor/test.rb
@@ -0,0 +1,22 @@
+#!/bin/ruby
+
+system('gcc -flto -O3 -s gray.c -o gray -D T_MAX=2 -lpthread') || raise
+
+['-1', '-2'].each do | m |
+    p = $*[0] || ?.
+    p += ?/ if p[-1] != ?/
+    sum = 0
+    Dir.entries(p).each do | f |
+        next unless f[/\.png$/]
+        system("convert #{p + f} /tmp/src.ppm") || (puts 'convert ' + f; raise)
+        system("./gray #{m} /tmp/src.ppm /tmp/res.gray > /dev/null") ||
+            (puts 'g_c ' + f; raise)
+        system('./gray -d /tmp/res.gray /tmp/res.ppm > /dev/null') ||
+            (puts 'g_d ' + f; raise)
+        sum += (z = File.size '/tmp/res.gray')
+        t = `sha256sum -b /tmp/src.ppm /tmp/res.ppm`.split
+        (puts 'g_sha256sum ' + f; raise) if t[0] != t[2]
+        # puts '%11d %s' % [z, f]
+    end
+    puts '%11d %s' % [sum, m]
+end
