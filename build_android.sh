6ef9768f6baf9e6d45ba1efffdada358ce9a566b
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-04-14 11:25:14 +0300

    Hobby Time: Day #6, Episode 2

---
+++ build_android.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/home/"$USER"/android_ndk/android-ndk-r23b/toolchains/llvm/prebuilt/\
+linux-x86_64/bin/aarch64-linux-android30-clang \
+-flto -O3 -s until_fork/*.c 7/libseven.c libxpng.c xpng.c -o xpng_android \
+-D T_MAX=4 -D SYNC_IO \
