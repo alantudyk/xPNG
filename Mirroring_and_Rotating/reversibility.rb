d4e7f7255a5463dad975a7475f52cb53f807b16d
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-11-05 08:23:57 +0300

    >>

--- Mirroring_and_Rotating/reversibility.rb
+++ Mirroring_and_Rotating/reversibility.rb
@@ -2,13 +2,18 @@
 
 system('./build.sh && cd ../7 && ./build.sh && cd ../Mirroring_and_Rotating') || exit(1)
 
-f = $*[0] || raise
-system("../7/seven --to_7 #{f} /tmp/s.7") || raise
-
-system('./tool --r90  /tmp/s.7 /tmp/x.7 && ' +
-       './tool --r270 /tmp/x.7 /tmp/r.7 && cmp /tmp/s.7 /tmp/r.7') || raise
-
-['mv', 'mh', 'mvh', 'tl', 'tr'].each do
-    system("./tool --#{_1} /tmp/s.7 /tmp/x.7 && " +
-           "./tool --#{_1} /tmp/x.7 /tmp/r.7 && cmp /tmp/s.7 /tmp/r.7") || raise
+['pigz-logo', 'olaf'].each do | f |
+    
+    f = '../images/' + f + '.png'
+    
+    system("../7/seven --to_7 #{f} /tmp/s.7") || raise
+    
+    system('./tool --r90  /tmp/s.7 /tmp/x.7 && ' +
+           './tool --r270 /tmp/x.7 /tmp/r.7 && cmp /tmp/s.7 /tmp/r.7') || raise
+    
+    ['mv', 'mh', 'mvh', 'tl', 'tr'].each do
+        system("./tool --#{_1} /tmp/s.7 /tmp/x.7 && " +
+               "./tool --#{_1} /tmp/x.7 /tmp/r.7 && cmp /tmp/s.7 /tmp/r.7") || raise
+    end
+    
 end
